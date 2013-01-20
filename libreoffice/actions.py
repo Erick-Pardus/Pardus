ce0ef16c92506c7feb640c275ecef1d574d33d01
namso-01 <osman.erkan@yandex.com>
2013-01-20 20:33:53 +0200

    libre edit

--- libreoffice/actions.py
+++ libreoffice/actions.py
@@ -7,14 +7,54 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure("--disable-debug \
-			 --disable-static \
-			 --disable-werror  \
-			 --with-pic 	    \
-			 --disable-spreadsheet-model")
+    shelltools.system("./autogen.sh --with-jdk-home=/opt/sun-jdk \
+				    --with-system-libs \
+				    --with-system-headers \
+				    --with-system-mozilla \
+				    --with-system-zlib \
+				    --without-system-nss \
+				    --with-system-mdds \
+				    --with-system-orcus \
+				    --with-system-xmlsec \
+				    --with-system-hypen \
+				    --with-system-libcmis \
+				    --with-system-libcdr \
+				    --without-system-clucene \
+				    --without-system-libmspub \
+				    --without-system-altlinuxhyph \
+				    --without-system-liblangtag \
+				    --without-system-jars \
+				    --without-system-hsqldb \
+				    --disable-mathmldtd \
+				    --disable-epm \
+				    --disable-gnome-vfs \
+				    --enable-gio \
+				    --enable-symbols \
+				    --enable-dbus \
+				    --enable-opengl \
+				    --enable-vba \
+				    --enable-gtk \
+				    --enable-ext-presenter-minimizer \
+				    --enable-ext-nlpsolver \
+				    --enable-ext-wiki-publisher \
+				    --with-servlet-api-jar=/usr/share/java/servlet-api.jar \
+				    --with-system-mythes \
+				    --with-system-dicts \
+				    --with-external-dict-dir=/usr/share/myspell \
+				    --without-myspell-dicts \
+				    --without-fonts \
+				    --without-ppds \
+				    --without-afms \
+				    --disable-gstreamer \
+				    --with-vendor='Pardus' \
+				    --enable-fetch-external ")
+
+    #--without-system-hsqldb \ libreoffice hsqldb 1.8.0 üzerini desteklemiyor.
 
 def build():
     autotools.make()
@@ -22,4 +62,5 @@ def build():
 def install():
     autotools.install()
 
-    pisitools.dodoc("AUTHORS", "COPYING","NEWS", "README")
+    pisitools.dodoc("COPYING", "README")
+
